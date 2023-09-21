d747926cae3cbc14b31e2db46b73c79e7c804036
Agustin Massieri <agmassieri00@gmail.com>
2023-09-21 12:54:54 -0300

    Boostrap applied on MainPage

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Drawer from '../components/Drawer';
-import MealList from '../components/MealList'
-import FoodList from '../components/FoodList'
+import MealList from '../components/MealList';
+import FoodList from '../components/FoodList';
+
 
 const Main = () => {
-
-  return(
-    <div>
-        <Drawer user={localStorage.getItem('username')}/>
-        <FoodList />
-        <MealList />
+  return (
+    <div className="container">
+      <Drawer user={localStorage.getItem('username')} />
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="row">
+            <div className="col-md-6">
+              <FoodList />
+            </div>
+            <div className="col-md-6">
+              <MealList />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  ); 
+  );
 }
 
 export default Main;
