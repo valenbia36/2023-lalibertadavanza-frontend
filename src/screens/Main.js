e462ce12f0230bf5aa5a927c219cc577e9ce17ec
Agustin Massieri <agmassieri00@gmail.com>
2023-10-04 20:04:17 -0300

    responsive main

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -5,16 +5,20 @@ import FoodList from "../components/FoodList";
 
 const Main = () => {
   return (
-    <div className="container">
-      <Drawer user={localStorage.getItem("username")} />
-      <div className="row justify-content-center align-items-center">
-        <div className="col-md-10 justify-content-center">
-          <div className="row justify-content-center">
-            <div className="col-md-4">
-              <FoodList />
-            </div>
-            <div className="col-md-8">
-              <MealList />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-1">
+          <Drawer user={localStorage.getItem("username")} />
+        </div>
+        <div className="col-md-11">
+          <div className="d-flex justify-content-center">
+            <div className="row">
+              <div className="col-md-4">
+                <FoodList />
+              </div>
+              <div className="col-md-8">
+                <MealList />
+              </div>
             </div>
           </div>
         </div>
