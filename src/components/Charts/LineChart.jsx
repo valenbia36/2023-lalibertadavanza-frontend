99d17f112667458ed6d20241a052212b996eabb2
Agustin Massieri <agmassieri00@gmail.com>
2023-10-08 18:56:46 -0300

    Linechart changes

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,27 +1,35 @@
 import React from "react";
-import { LineChart } from "@mui/x-charts/LineChart";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend } from "recharts";
 
-export default function BasicLineChart(data) {
-  
-  const datesNumeric = (data.data).map(item => parseInt(item.date, 10));
-  const caloriasArray = (data.data).map(item => item.calorias);
+export default function LineChartWithCustomFontSize(data) {
+  const caloriasArray = data.data.map((item) => item.calorias);
 
-  const xAxisConfig = {
-    max: 31, // Aquí puedes establecer el valor máximo deseado en el eje X
-  };
+  const totalCalorias = caloriasArray.reduce((acc, curr) => acc + curr, 0);
+  const promedioCalorias = totalCalorias / caloriasArray.length;
+
+  const dataWithAverage = data.data.map((item) => ({
+    date: item.date,
+    calorias: item.calorias,
+    promedio: promedioCalorias,
+  }));
 
   return (
-    <div style={{ marginTop: '-25%' }}>
+    <div style={{ fontSize: 12  }}>
       <LineChart
-        xAxis={[{ data: datesNumeric, ...xAxisConfig }]}
-        series={[
-          {
-            data: caloriasArray,
-          },
-        ]}
         width={320}
         height={450}
-      />
+        data={dataWithAverage}
+        margin={{ top: 10, right: 30, left: 20, bottom: 30 }}
+        s
+      >
+        <Legend verticalAlign="bottom" iconSize={25} layout="vertical" margin={{top: 1000}}/>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" label={{ value: "Fecha", position: 'bottom', fontSize: 14, fontWeight: 'bold' }} />
+        <YAxis label={{ value: "Calorías", angle: -90, position: "insideLeft", fontSize: 14, fontWeight: 'bold' }} />
+        <Legend verticalAlign="top" width={100} />
+        <Line type="monotone" dataKey="calorias" name="Calorías" stroke="#936639" strokeWidth={2} dot={false}/>
+        <Line type="monotone" dataKey="promedio" name={`Promedio de Calorías (${promedioCalorias.toFixed(2)})`} stroke="#6a994e" dot={false} strokeWidth={2} strokeDasharray="3 3"/>
+      </LineChart>
     </div>
   );
 }
