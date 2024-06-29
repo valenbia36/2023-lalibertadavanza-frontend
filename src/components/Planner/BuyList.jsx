639d7b4f67f853b7e0e58ff075747dcfca0425f4
valenbia36 <vbianchi16@gmail.com>
2024-06-29 15:35:02 -0300

    Shopping list refactor

--- src/components/Planner/BuyList.jsx
+++ src/components/Planner/BuyList.jsx
@@ -1,39 +1,25 @@
-import React from "react";
-import { Typography, List, ListItem, ListItemText } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+} from "@mui/material";
 
 const BuyList = ({ shoppingListData, weeklyTotalPerFood }) => {
-  const daysOfWeek = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
+  const [purchaseAmount, setPurchaseAmount] = useState({});
 
-  const calculateTotalWeightConsumedPerDay = (data) => {
-    const totalWeightConsumedPerDay = {};
+  const calculateWeeklyTotalConsumed = (data) => {
     const weeklyTotalConsumed = {};
-    /*  console.log(data); */
 
     // Iterate over each day
     Object.keys(data).forEach((day) => {
-      totalWeightConsumedPerDay[day] = {};
-
       // Iterate over each meal (breakfast, lunch, dinner)
       Object.keys(data[day] || {}).forEach((meal) => {
         // Iterate over each food item in the meal
         (data[day][meal] || []).forEach((food) => {
           const foodName = food.foodId.name;
-
-          // Initialize total weight consumed for the food if not present
-          if (!totalWeightConsumedPerDay[day][foodName]) {
-            totalWeightConsumedPerDay[day][foodName] = 0;
-          }
-
-          // Increment total weight consumed for the food on the specific day
-          totalWeightConsumedPerDay[day][foodName] += food.weightConsumed;
 
           // Initialize weekly total weight consumed for the food if not present
           if (!weeklyTotalConsumed[foodName]) {
@@ -46,50 +32,42 @@ const BuyList = ({ shoppingListData, weeklyTotalPerFood }) => {
       });
     });
 
-    return { totalWeightConsumedPerDay, weeklyTotalConsumed };
+    return weeklyTotalConsumed;
+  };
+
+  const handlePurchaseChange = (event, foodName) => {
+    const { value } = event.target;
+    const updatedPurchaseAmount = {
+      ...purchaseAmount,
+      [foodName]: value,
+    };
+    setPurchaseAmount(updatedPurchaseAmount);
   };
 
   // Example usage:
-  const { totalWeightConsumedPerDay, weeklyTotalConsumed } =
-    calculateTotalWeightConsumedPerDay(shoppingListData);
-  /* console.log(totalWeightConsumedPerDay); */
+  const weeklyTotalConsumed = calculateWeeklyTotalConsumed(shoppingListData);
+
   return (
     <div>
-      {/* Mostrar la suma total de cada alimento por día */}
-      <Typography variant="h6" gutterBottom>
-        Shopping List:
-      </Typography>
-
-      {daysOfWeek.map((day, index) => (
-        <div key={index}>
-          <Typography variant="subtitle1" gutterBottom>
-            {`${day}:`}
-          </Typography>
-          <List>
-            {Object.keys(totalWeightConsumedPerDay[day] || {}).map(
-              (foodName, foodIndex) => (
-                <ListItem key={foodIndex}>
-                  <ListItemText
-                    primary={`${foodName}: ${totalWeightConsumedPerDay[day][foodName]} grams/ml`}
-                  />
-                </ListItem>
-              )
-            )}
-          </List>
-        </div>
-      ))}
-
       {/* Mostrar la suma total de cada alimento para toda la semana */}
       <Typography variant="h6" gutterBottom>
         Weekly Total:
       </Typography>
-      <ul>
+      <List>
         {Object.keys(weeklyTotalConsumed).map((foodName, index) => (
-          <li key={index}>
-            <Typography>{`${foodName}: ${weeklyTotalConsumed[foodName]} grams/ml`}</Typography>
-          </li>
+          <ListItem key={index}>
+            <ListItemText
+              primary={`${foodName}: ${weeklyTotalConsumed[foodName]} grams/ml`}
+            />
+            <TextField
+              label="Purchased Amount"
+              type="number"
+              value={purchaseAmount[foodName] || ""}
+              onChange={(event) => handlePurchaseChange(event, foodName)}
+            />
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
